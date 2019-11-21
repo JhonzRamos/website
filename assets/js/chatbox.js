c9dc63c3c8f37baf7f48c17cc63c1ccd3c99d2ad
Jhon Zylvin Ramos <ramosjhonzylvin@gmail.com>
2019-11-21 09:34:19 +0800

    Add files via upload

---
+++ assets/js/chatbox.js
@@ -0,0 +1,9 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+    document.document.getElementsByClassName("b-agent-demo_header-wrapper").style.display = "none";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  
+}
